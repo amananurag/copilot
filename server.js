f562fc662db2c5ed465b2b91cdc57ee6b05ea0eb
aman anurag <aman.anurag07>
2023-05-29 09:58:25 +0530

    key in obj

--- server.js
+++ server.js
@@ -33,3 +33,24 @@ delete person.name;
 console.log(person.name);
 console.log('name' in person);
 console.log('age' in person);
+
+// create a custom object   
+const person1 = {
+    x: 'John',
+    y: 22
+};
+
+// write a function to loop the keys of the above object    
+function loopKeys(obj) {    
+    for (let key in obj) {
+        console.log(key);
+    }
+    
+}
+
+const has = (obj, key) => {
+    return key in obj;
+}
+
+console.log(loopKeys(person1));
+console.log(has(person1, 'x'));
