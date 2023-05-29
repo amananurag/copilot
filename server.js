a188ae67d196d22c47669580dce46380277b9294
aman anurag <aman.anurag07>
2023-05-29 09:28:16 +0530

    added proxy object in js

---
+++ server.js
@@ -0,0 +1,35 @@
+// run node server on port 8080
+const person = new Proxy({ age: 22}, {
+    set(target, key, value) {
+        console.log(`setting  ${key} to ${value}`)
+        if(key =='name') {
+            target[key] = value
+        }else{
+            throw new Error('You cannot set age to person');
+        }
+    },
+    get(target, key) {
+        console.log(`getting ${key}`)
+        return target[key]
+    },
+    deleteProperty(target, key) {    
+        if(key == 'name') {
+
+        console.log(`deleting ${key}`)
+        delete target[key];
+        }else{
+            throw new Error('You cannot delete age from person');
+        }
+        },
+    has(target, key) {
+        console.log(`has ${key}`)
+        return key in target
+    }
+})
+
+person.name = 'John';
+console.log(person.name);
+delete person.name;
+console.log(person.name);
+console.log('name' in person);
+console.log('age' in person);
