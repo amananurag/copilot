f61b891615e907607b714b2cff9d1a5896c49e70
aman anurag <aman.anurag07>
2023-05-31 17:03:30 +0530

    prettier added

--- server.js
+++ server.js
@@ -1,88 +1,86 @@
 // run node server on port 8080
-const person = new Proxy({ age: 22}, {
+const person = new Proxy(
+  { age: 22 },
+  {
     set(target, key, value) {
-        console.log(`setting  ${key} to ${value}`)
-        if(key =='name') {
-            target[key] = value
-        }else{
-            throw new Error('You cannot set age to person');
-        }
+      console.log(`setting  ${key} to ${value}`);
+      if (key == "name") {
+        target[key] = value;
+      } else {
+        throw new Error("You cannot set age to person");
+      }
     },
     get(target, key) {
-        console.log(`getting ${key}`)
-        return target[key]
+      console.log(`getting ${key}`);
+      return target[key];
     },
-    deleteProperty(target, key) {    
-        if(key == 'name') {
-
-        console.log(`deleting ${key}`)
+    deleteProperty(target, key) {
+      if (key == "name") {
+        console.log(`deleting ${key}`);
         delete target[key];
-        }else{
-            throw new Error('You cannot delete age from person');
-        }
-        },
+      } else {
+        throw new Error("You cannot delete age from person");
+      }
+    },
     has(target, key) {
-        console.log(`has ${key}`)
-        return key in target
-    }
-})
+      console.log(`has ${key}`);
+      return key in target;
+    },
+  }
+);
 
-person.name = 'John';
+person.name = "John";
 console.log(person.name);
 delete person.name;
 console.log(person.name);
-console.log('name' in person);
-console.log('age' in person);
+console.log("name" in person);
+console.log("age" in person);
 
-// create a custom object   
+// create a custom object
 const person1 = {
-    x: 'John',
-    y: 22
+  x: "John",
+  y: 22,
 };
 
-// write a function to loop the keys of the above object    
-function loopKeys(obj) {    
-    for (let key in obj) {
-        console.log(key);
-    }
-    
+// write a function to loop the keys of the above object
+function loopKeys(obj) {
+  for (let key in obj) {
+    console.log(key);
+  }
 }
 
 const has = (obj, key) => {
-    return key in obj;
-}
+  return key in obj;
+};
 
 console.log(loopKeys(person1));
-console.log(has(person1, 'x'));
-
+console.log(has(person1, "x"));
 
 // use express to create a server
-const express = require('express');
+const express = require("express");
 
-
-const swagger = require('./swagger');
+const swagger = require("./swagger");
 const app = express();
 
-require('./swagger')(app);
+require("./swagger")(app);
+
 /**
  * @swagger
  * /:
  *   get:
- *     summary: Get a greeting message
- *     description: Retrieve a greeting message from the server
  *     responses:
  *       200:
- *         description: A greeting message
+ *         description: Welcome String
  *         content:
- *           text/plain:
+ *           application/json:
  *             schema:
  *               type: string
+ *
  */
-app.get('/', (req, res) => {
-    res.send('Hello World from express server');
-  });
-  
-app.listen(8080, () => {
-    console.log('Server is running');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Hello welcome from Express Server");
+});
+
+        app.listen(8080, () => {
+  console.log("Server is running");
+});
