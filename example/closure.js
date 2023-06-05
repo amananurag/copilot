3b4806a5e3f115f7e2fba669421bb40dc19d2629
aman anurag <aman.anurag07>
2023-06-05 16:14:42 +0530

    interview question added

--- example/closure.js
+++ example/closure.js
@@ -1,7 +1,6 @@
 function outer() {
   let counter = 0;
   function inner() {
-    console.log("counter", counter);
     counter++;
     return counter;
   }
@@ -17,3 +16,36 @@ run();
 const run2 = outer();
 run2();
 run2();
+
+function closure() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+
+closure();
+
+function closure1() {
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+
+closure1();
+
+function closure2() {
+  for (var i = 1; i <= 5; i++) {
+    function inner(i) {
+      setTimeout(function () {
+        console.log(i);
+      }, i * 1000);
+    }
+    inner(i);
+  }
+}
+
+closure2();
