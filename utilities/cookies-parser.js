025746b700987d7e0f3c6ce0e843e78b0cd38dc7
aman anurag <aman.anurag07>
2023-07-03 12:42:18 +0530

    added comment

--- utilities/cookies-parser.js
+++ utilities/cookies-parser.js
@@ -1,12 +1,15 @@
 function cookiesParser(cookies) {
   let session_id = "";
+  // res.headers.cookie or cookies ( single string separated by ;) = ["cookie_1=value_1; cookie_2=value_2; session_id=1234"]
   if (cookies) {
     const cookieArray = cookies.split(";");
+    // ['cookie_1=value_1','cookie_2=value_2', 'session_id=1234']
     console.log("cookieArray:", cookieArray);
     const cookieObj = {};
 
     for (let i = 0; i < cookieArray.length; i++) {
-      const cookie = cookieArray[i].trim().split("=");
+      const cookie = cookieArray[i].trim().split("="); // ['cookie_1', 'value_1'] ['cookie_2', 'value_2'] ['session_id', '1234']
+      // [cookie_1] = [value_1];
       cookieObj[cookie[0]] = cookie[1];
     }
 
